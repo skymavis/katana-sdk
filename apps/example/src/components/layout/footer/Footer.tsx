d65ea75b0d6da92d09df3b2531a6d8739f3e69cb
Minh Trần <117151487+minhtranlc@users.noreply.github.com>
2025-03-06 15:51:51 +0700

    Delete apps/example directory

--- apps/example/src/components/layout/footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-
-import styles from './Footer.module.scss';
-
-const Footer: FC = () => {
-  return <footer className={styles.footer}>@minhtlc.ron</footer>;
-};
-export default Footer;
