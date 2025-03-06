b739512f50a052286b40a3cd4514051e8cd8f144
Minh Trần <117151487+minhtranlc@users.noreply.github.com>
2025-03-06 15:51:51 +0700

    Delete apps/example directory

--- apps/example/src/hooks/useGetRuntimeChainId.ts
+++
@@ -1,8 +0,0 @@
-import { SupportedChainIds } from '@roninnetwork/walletgo';
-import getConfig from 'next/config';
-
-export const useGetRuntimeChainId = () => {
-  const { publicRuntimeConfig } = getConfig();
-  const { chainId } = publicRuntimeConfig;
-  return parseInt(chainId ?? SupportedChainIds.RoninMainet);
-};
